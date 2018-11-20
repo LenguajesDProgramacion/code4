e37e8fd8c784a26e1187f40e15796941a7a1d742
Laboratorio1Pc2 <Laboratorio1Pc2@UCATEC.local>
2018-11-19 20:03:49 -0400

    Agregar Interfaz

---
+++ src/main/java/Vehiculo.java
@@ -0,0 +1,10 @@
+public class Vehiculo implements Ejemplo {
+
+    public void moverRueda(){
+        System.out.println("mover rueda del vehiculo");
+    }
+
+    public void estadoRueda(){
+        System.out.println("estado en que se encuentra el vehiculo");
+    }
+}
